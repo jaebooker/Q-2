2241f3f95e8c55aa6dce7b4e78cfcca0b8c67b19
jaebooker <jaesonbooker@gmail.com>
2019-04-22 14:33:14 -0700

    first blood

---
+++ Q-2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Q-2
+//
+//  Created by Jaeson Booker on 4/22/19.
+//  Copyright © 2019 Jaeson Booker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
